e5703a637f0cb47749272d6f75e36f26dfb4184f
egoossens <edouard.goossens@gmail.com>
2020-08-04 18:25:19 +0200

    use NSInteger instead of NSNumber for playerId and recorderId

--- ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioPlayer.m
+++ ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioPlayer.m
@@ -4,35 +4,35 @@
 
 @interface RCT_EXTERN_MODULE(AudioPlayer, NSObject)
 
-RCT_EXTERN_METHOD(prepare:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(prepare:(nonnull NSInteger*)playerId
                   withPath:(NSString* _Nullable)path
                   withOptions:(NSDictionary *)options
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(seek:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(seek:(nonnull NSInteger*)playerId
                   withPos:(nonnull NSNumber*)position
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(set:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(set:(nonnull NSInteger*)playerId
                   withOptions:(NSDictionary*)options
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(destroy:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(destroy:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(play:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(play:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(stop:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(stop:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(pause:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(pause:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(resume:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(resume:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(getCurrentTime:(nonnull NSNumber*)playerId
+RCT_EXTERN_METHOD(getCurrentTime:(nonnull NSInteger*)playerId
                   withCallback:(RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(test)
--- ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioRecorder.m
+++ ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioRecorder.m
@@ -3,21 +3,21 @@
 
 @interface RCT_EXTERN_MODULE(AudioRecorder, NSObject)
 
-RCT_EXTERN_METHOD(prepare:(nonnull NSNumber *)recorderId
+RCT_EXTERN_METHOD(prepare:(nonnull NSInteger *)recorderId
                   withPath:(NSString * _Nullable)filename
                   withOptions:(NSDictionary *)options
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(record:(nonnull NSNumber *)recorderId
+RCT_EXTERN_METHOD(record:(nonnull NSInteger *)recorderId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(stop:(nonnull NSNumber *)recorderId
+RCT_EXTERN_METHOD(stop:(nonnull NSInteger *)recorderId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(pause:(nonnull NSNumber *)recorderId
+RCT_EXTERN_METHOD(pause:(nonnull NSInteger *)recorderId
                   withCallback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(destroy:(nonnull NSNumber *)recorderId
+RCT_EXTERN_METHOD(destroy:(nonnull NSInteger *)recorderId
                   withCallback:(RCTResponseSenderBlock)callback)
 
 @end
